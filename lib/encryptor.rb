c13a1c0810744acb1e07513b54858f6c75373e9c
Allan <amaxwellblair@gmail.com>
2016-01-03 15:54:29 -0700

    completed encrypt class

---
+++ lib/encryptor.rb
@@ -0,0 +1,44 @@
+$LOAD_PATH.unshift(__dir__)
+require 'pry'
+require 'alphabet'
+
+class Encryptor
+  include Alphabet
+  attr_accessor :key, :date, :string
+
+  def initialize(args = {})
+    @key = args.fetch(:key, "00000")
+    @date = date_initialize(args.fetch(:date, "000000"))
+    @string = args.fetch(:string, "you did not input a string")
+  end
+
+  def date_initialize(date)
+    (date**2).to_s[-4..-1]
+  end
+
+  def modulo(range, num)
+    if num > range.last
+      modulo(range, num - (range.count))
+    elsif num < range.first
+      modulo(range, num + (range.count))
+    else
+      num
+    end
+  end
+
+  def encrypt
+    string.chars.map.with_index do |chr, i|
+      k = ALPH[chr] + rotation(modulo(0..3, i)) + offset(modulo(0..3, i))
+      NUM[modulo(0..ALPH.length - 1, k)]
+    end.join
+  end
+
+  def rotation(index)
+    key[index..index+1].to_i
+  end
+
+  def offset(index)
+    date[index].to_i
+  end
+
+end
--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -3,23 +3,69 @@ SimpleCov.start
 require 'minitest'
 require 'encryptor'
 
-class EncryptorTest < Minitest::test
+class EncryptorTest < Minitest::Test
   attr_reader :encrypt
 
   def setup
-    args = {key: "12345", date: 010116, string: "hola"}
+    args = {:key => "12345", :date => 010116, :string => "a"}
     @encrypt = Encryptor.new(args)
   end
 
   def test_class_exists
-    skip
     assert_equal Encryptor, encrypt.class
   end
 
   def test_date_initialize
-    assert_equal 010116**2[-4..-1], encrypt.date
+    assert_equal (010116**2).to_s[-4..-1], encrypt.date
+    #"17422276"
   end
 
-  def 
+  def test_alphabet_is_valid
+    assert_equal 0, Encryptor::ALPH["a"]
+    assert_equal 25, Encryptor::ALPH["z"]
+    assert_equal 26, Encryptor::ALPH[" "]
+  end
+
+  def test_rotation
+    assert_equal 12, encrypt.rotation(0)
+  end
+
+  def test_offset
+    assert_equal 2, encrypt.offset(0)
+  end
+
+  def test_modulo
+    range = 0..3
+    num = 6
+    assert_equal 2, encrypt.modulo(range, num)
+  end
+
+  def test_encrypt_one_letter
+    assert_equal "o", encrypt.encrypt
+  end
+
+  def test_encrypt_two_letters
+    args = {:key => "12345", :date => 010116, :string => "aa"}
+    @encrypt = Encryptor.new(args)
+    assert_equal "oz", encrypt.encrypt
+  end
+
+  def test_encrypt_three_letters
+    args = {:key => "12345", :date => 010116, :string => "aaa"}
+    @encrypt = Encryptor.new(args)
+    assert_equal "ozo", encrypt.encrypt
+  end
+
+  def test_encrypt_four_letters
+    args = {:key => "12345", :date => 010116, :string => "aaaa"}
+    @encrypt = Encryptor.new(args)
+    assert_equal "ozoy", encrypt.encrypt
+  end
+
+  def test_encrypt_sentence
+    args = {:key => "12345", :date => 010116, :string => "aaaa aaa"}
+    @encrypt = Encryptor.new(args)
+    assert_equal "ozoynzoy", encrypt.encrypt
+  end
 
 end
